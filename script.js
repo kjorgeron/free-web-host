87672ceac82fb508eadf5da8e38c859356080e0b
Kevin <kjorgeron0819@gmail.com>
2025-04-12 20:41:01 -0400

    first setup of mobile menu button

--- script.js
+++ script.js
@@ -5,16 +5,23 @@ hello.innerHTML = "<p class='hello'>Hello World!<br>From your local JS!</p>"
 
 
 
-  // Select all buttons within the action bar
-  const buttons = document.querySelectorAll('.action_bar button');
+// Select all buttons within the action bar
+const buttons = document.querySelectorAll('.action_bar button');
 
-  // Add click event listener to each button
-  buttons.forEach(button => {
+// Add click event listener to each button
+buttons.forEach(button => {
     button.addEventListener('click', () => {
-      // Remove 'active' class from all buttons
-      buttons.forEach(btn => btn.classList.remove('active'));
+        // Remove 'active' class from all buttons
+        buttons.forEach(function(btn){
+            if (btn.className != "mobile_menu") {
+                btn.classList.remove('active')
+            }
+        })
+        // buttons.forEach(btn => btn.classList.remove('active'));
 
-      // Add 'active' class to the clicked button
-      button.classList.add('active');
+        // Add 'active' class to the clicked button
+        if (button.className != "mobile_menu"){
+            button.classList.add('active');
+        }
     });
-  });
+});
