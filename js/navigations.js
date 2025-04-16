296701068b7ddbb3cd4a03b674e7c58b2cc628ba
Kevin <kjorgeron0819@gmail.com>
2025-04-15 22:51:51 -0400

    added dark and light modes

--- js/navigations.js
+++ js/navigations.js
@@ -1,14 +1,16 @@
+
 // Set up action bar buttons
-action_bar = document.querySelector('.action_bar')
+let action_bar = document.querySelector('.action_bar')
 action_bar.innerHTML = `
-        <div class="logo"></div>
-        <div class="nav_links">
-        <button class="nav active">Home</button>
-        <button class="nav">About</button>
-        <button class="nav">Portfolio</button>
-        <button class="nav">Contact</button>
-        <button class="mobile_menu">≡</button>
-        </div>
+<div class="logo"></div>
+<div class="nav_links">
+<div class="toggle_mode"><div class="switch"></div></div>
+<button class="nav active">Home</button>
+<button class="nav">About</button>
+<button class="nav">Portfolio</button>
+<button class="nav">Contact</button>
+<button class="mobile_menu">≡</button>
+</div>
 `
 logo = document.querySelector('.logo')
 logo.innerHTML = `<img src="images/software-engineer-logo-darkmode.png" alt="Logo"/>`
@@ -18,24 +20,24 @@ const buttons = document.querySelectorAll('.nav');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-
+        
         // Remove active class from all buttons
         buttons.forEach(btn => {
             btn.classList.remove('active');
         });
-
+        
         // Add active class to button pressed
         button.classList.add("active");
-
+        
     });
 });
+        
 
 const mobile_button = document.querySelector('.action_bar .nav_links .mobile_menu');
-
 mobile_button.addEventListener('click', (event) => {
     // Toggle innerHTML between "≡" and "X"
     event.target.innerHTML = event.target.innerHTML === "X" ? "≡" : "X";
-
+    
     // Toggle 'active' class based on innerHTML
     if (event.target.innerHTML === "X") {
         event.target.classList.add("active");
@@ -43,3 +45,45 @@ mobile_button.addEventListener('click', (event) => {
         event.target.classList.remove("active");
     }
 });
+
+
+// THEME SWITCH FOR DARK & LIGHT MODES
+const body = document.querySelector('body')
+const switch_mode = document.querySelector('.toggle_mode')
+const icon = document.querySelector('.switch')
+const root = document.documentElement;
+const active = document.querySelector('.active')
+switch_mode.addEventListener('click', event => {
+    console.log('click')
+    const icon_bg = window.getComputedStyle(icon).backgroundColor
+    console.log(icon_bg);
+    
+    switch(icon_bg){
+        case "rgb(255, 255, 255)": // white
+        switch_mode.style.justifyContent = "flex-start"
+        switch_mode.style.borderColor = "blue"
+        active.style.borderColor = "blue"
+        active.style.color = "blue"
+        root.style.setProperty('--theme', 'blue')
+        root.style.setProperty('--background_color', 'white')
+        root.style.setProperty('--border_color', 'black')
+        root.style.setProperty('--font_color', 'black')
+        icon.style.backgroundColor = "black"
+        logo.innerHTML = `<img src="images/software-engineer-logo-lightmode.png" alt="Logo"/>`
+        break
+        case "rgb(0, 0, 0)": // black
+        switch_mode.style.justifyContent = "flex-end"
+        switch_mode.style.borderColor = "red"
+        active.style.borderColor = "red"
+        active.style.color = "red"
+        root.style.setProperty('--theme', 'red')
+        root.style.setProperty('--background_color', 'black')
+        root.style.setProperty('--border_color', 'white')
+        root.style.setProperty('--font_color', 'white')
+        icon.style.backgroundColor = "white"
+        logo.innerHTML = `<img src="images/software-engineer-logo-darkmode.png" alt="Logo"/>`
+        break
+        default:
+            break
+        }
+})
