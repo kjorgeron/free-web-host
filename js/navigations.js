4ad3ba3d42138d1fe2dd4f80ab1fd25e8acb3103
Kevin <kjorgeron0819@gmail.com>
2025-04-15 22:53:17 -0400

    remove html folder... will render the html in js and reformat

--- js/navigations.js
+++ js/navigations.js
@@ -20,24 +20,24 @@ const buttons = document.querySelectorAll('.nav');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        
+
         // Remove active class from all buttons
         buttons.forEach(btn => {
             btn.classList.remove('active');
         });
-        
+
         // Add active class to button pressed
         button.classList.add("active");
-        
+
     });
 });
-        
+
 
 const mobile_button = document.querySelector('.action_bar .nav_links .mobile_menu');
 mobile_button.addEventListener('click', (event) => {
     // Toggle innerHTML between "≡" and "X"
     event.target.innerHTML = event.target.innerHTML === "X" ? "≡" : "X";
-    
+
     // Toggle 'active' class based on innerHTML
     if (event.target.innerHTML === "X") {
         event.target.classList.add("active");
@@ -57,33 +57,33 @@ switch_mode.addEventListener('click', event => {
     console.log('click')
     const icon_bg = window.getComputedStyle(icon).backgroundColor
     console.log(icon_bg);
-    
-    switch(icon_bg){
+
+    switch (icon_bg) {
         case "rgb(255, 255, 255)": // white
-        switch_mode.style.justifyContent = "flex-start"
-        switch_mode.style.borderColor = "blue"
-        active.style.borderColor = "blue"
-        active.style.color = "blue"
-        root.style.setProperty('--theme', 'blue')
-        root.style.setProperty('--background_color', 'white')
-        root.style.setProperty('--border_color', 'black')
-        root.style.setProperty('--font_color', 'black')
-        icon.style.backgroundColor = "black"
-        logo.innerHTML = `<img src="images/software-engineer-logo-lightmode.png" alt="Logo"/>`
-        break
+            switch_mode.style.justifyContent = "flex-start"
+            switch_mode.style.borderColor = "blue"
+            active.style.borderColor = "blue"
+            active.style.color = "blue"
+            root.style.setProperty('--theme', 'blue')
+            root.style.setProperty('--background_color', 'white')
+            root.style.setProperty('--border_color', 'black')
+            root.style.setProperty('--font_color', 'black')
+            icon.style.backgroundColor = "black"
+            logo.innerHTML = `<img src="images/software-engineer-logo-lightmode.png" alt="Logo"/>`
+            break
         case "rgb(0, 0, 0)": // black
-        switch_mode.style.justifyContent = "flex-end"
-        switch_mode.style.borderColor = "red"
-        active.style.borderColor = "red"
-        active.style.color = "red"
-        root.style.setProperty('--theme', 'red')
-        root.style.setProperty('--background_color', 'black')
-        root.style.setProperty('--border_color', 'white')
-        root.style.setProperty('--font_color', 'white')
-        icon.style.backgroundColor = "white"
-        logo.innerHTML = `<img src="images/software-engineer-logo-darkmode.png" alt="Logo"/>`
-        break
+            switch_mode.style.justifyContent = "flex-end"
+            switch_mode.style.borderColor = "red"
+            active.style.borderColor = "red"
+            active.style.color = "red"
+            root.style.setProperty('--theme', 'red')
+            root.style.setProperty('--background_color', 'black')
+            root.style.setProperty('--border_color', 'white')
+            root.style.setProperty('--font_color', 'white')
+            icon.style.backgroundColor = "white"
+            logo.innerHTML = `<img src="images/software-engineer-logo-darkmode.png" alt="Logo"/>`
+            break
         default:
             break
-        }
+    }
 })
